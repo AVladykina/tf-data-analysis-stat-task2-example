c377df426fa37c216cd2a393089c15f24c227be6
AVladykina <55153864+AVladykina@users.noreply.github.com>
2023-04-05 23:51:18 -0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,12 +7,13 @@ from scipy.stats import norm
 chat_id = 433242632 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    a = 0.056  # левая граница равномерного распределения
-    b = np.max(x)  # правая граница равномерного распределения
-    delta = (b - a) * np.sqrt(1 - p) / 2
-    return a + delta, b - delta
-#     alpha = 1 - p
-#     loc = x.mean()
-#     scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-#     return loc - scale * norm.ppf(1 - alpha / 2), \
-#            loc - scale * norm.ppf(alpha / 2)
+ # Вычисляем оценки параметров
+    loc = x.mean() - 0.056 / 2
+    scale = np.sqrt(12) * (x.std() + 0.056 / 2) / np.sqrt(len(x))
+    
+    # Вычисляем доверительный интервал
+    alpha = 1 - p
+    left = loc - scale * norm.ppf(1 - alpha / 2)
+    right = loc + scale * norm.ppf(1 - alpha / 2)
+    
+    return left, right
